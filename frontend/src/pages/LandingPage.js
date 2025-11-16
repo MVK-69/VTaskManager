0371f025daecb6bc121698c03db62516bfd30a55
MVK-69 <mvk11@iitbbs.ac.in>
2025-11-17 00:12:27 +0530

    added LandingPage js and css files

---
+++ frontend/src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LandingPage.css";
+
+function LandingPage() {
+
+    return (
+        <div className="landing-container">
+            <h1 className="landing-title">Welcome to VTaSk Manager</h1>
+        </div>
+    );
+}
+
+export default LandingPage;
