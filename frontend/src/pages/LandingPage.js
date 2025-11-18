5f3ef25c16fb3c93e59369bac7444e8b42ba7e1d
MVK-69 <mvk11@iitbbs.ac.in>
2025-11-18 10:25:42 +0530

    added logic for LOGIN and REGISTER buttons in Landing Page

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./LandingPage.css";
 
 function LandingPage() {
 
+    const [showButtons, setShowButtons] = useState(false);
+    const navigate = useNavigate();
+
+    //show buttons after title animation (2 seconds)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowButtons(true);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div className="landing-container">
             <h1 className="landing-title">Welcome to VTaSk Manager</h1>
+
+            {showButtons && (
+                <div className="landing-buttons">
+                    <button onClick={() => navigate("/login")} className="btn landing-btn">LOGIN</button>
+                    <button onClick={() => navigate("/register")} className="btn landing-btn">REGISTER</button>
+                </div>
+            )}
         </div>
     );
 }
